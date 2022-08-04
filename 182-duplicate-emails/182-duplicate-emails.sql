19695102caa2c2b7a6c225ac0c9b3647653c3c5f
Nandhakumar T <80043272+nandhakumarT@users.noreply.github.com>
2022-08-04 15:59:40 +0530

    Time: 457 ms (47.63%), Space: 0B (100.00%) - LeetHub

--- 182-duplicate-emails/182-duplicate-emails.sql
+++ 182-duplicate-emails/182-duplicate-emails.sql
@@ -1,14 +1,14 @@
 # Write your MySQL query statement below
 
-SELECT email AS Email
-FROM(
-SELECT email, COUNT(email) AS c
-FROM Person
-GROUP BY email) AS temp
-WHERE c>1
-
-
-#SELECT DISTINCT email AS Email
+#SELECT email AS Email
+#FROM(
+#SELECT email, COUNT(email) AS c
 #FROM Person
-#GROUP BY id
-#HAVING email
+#GROUP BY email) AS temp
+#WHERE c>1
+
+
+SELECT email AS Email
+FROM Person
+GROUP BY email
+HAVING COUNT(email)>1
