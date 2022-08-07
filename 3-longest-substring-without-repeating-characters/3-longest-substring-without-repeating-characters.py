fa120278726b5e6229f851f53d83d6ea2c7a22ee
Nandhakumar T <80043272+nandhakumarT@users.noreply.github.com>
2022-08-07 22:27:41 +0530

    Time: 92 ms (65.26%), Space: 14.1 MB (49.55%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -0,0 +1,23 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        
+        #We have to use SET for this Question
+        #Siliding Window technique
+        #l,r pointers l=0,r is keeping moving
+        #if any dulipate in set then remove it from left and then increment left pointer by one
+        #if not then add right to charset
+        #res should be r+l-1
+        
+        charset=set()
+        l=0
+        res=0
+        for r in range(len(s)):
+            while s[r] in charset:
+                charset.remove(s[l])
+                l+=1
+            charset.add(s[r])
+            res=max(res,r-l+1)
+        return res
+                    
+                    
+        
