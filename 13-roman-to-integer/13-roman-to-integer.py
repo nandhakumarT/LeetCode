ba3dab303271dd4d5ec61791ffd0bffde43da649
Nandhakumar T <80043272+nandhakumarT@users.noreply.github.com>
2022-08-08 17:27:06 +0530

    Time: 112 ms (7.86%), Space: 13.8 MB (76.66%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -3,6 +3,9 @@ class Solution:
         charset={'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
         res=0
         
+        if len(s)==0:
+            return 
+        
         for i in range(len(s)):
             
             if i+1 < len(s) and charset[s[i]]<charset[s[i+1]] :
