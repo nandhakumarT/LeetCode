9715a67d472907cfb2a9b2a5c6c632fc910375b5
Nandhakumar T <80043272+nandhakumarT@users.noreply.github.com>
2022-08-08 15:51:17 +0530

    Time: 1603 ms (5.20%), Space: 0B (100.00%) - LeetHub

--- 262-trips-and-users/262-trips-and-users.sql
+++ 262-trips-and-users/262-trips-and-users.sql
@@ -3,7 +3,7 @@
 SELECT DISTINCT request_at AS 'Day', 
 ROUND(SUM(IF(status != "completed",1,0))/COUNT(status),2) AS "Cancellation Rate"
 FROM Trips 
-WHERE request_at <="2013-10-03" AND request_at>="2013-10-01"
+WHERE request_at BETWEEN "2013-10-01" AND "2013-10-03"
 AND client_id NOT IN (SELECT users_id FROM Users WHERE banned ="yes")
 AND driver_id NOT IN (SELECT users_id FROM Users WHERE banned ="yes")
 GROUP BY request_at
