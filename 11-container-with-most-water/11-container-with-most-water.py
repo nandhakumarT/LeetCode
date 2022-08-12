da4c10b63c862593366661c59e3280b9a4d8610b
Nandhakumar T <80043272+nandhakumarT@users.noreply.github.com>
2022-08-12 14:49:32 +0530

    Time: 968 ms (68.59%), Space: 27.5 MB (15.08%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        
+        #edge case
         h=height
         if len(h)==0:
             return 0
@@ -8,9 +8,14 @@ class Solution:
             return h
         
         l,r=0,len(h)-1
-        res=0
         maxwater=0
         
+#by find the min height between the left and right pointer
+
+#then find the distance between the left and right pointer by subtracting the left pointer vlaue and right pointer value
+
+#then multiple both min height and maxdistance and find the max value of individual result
+
         while l<r:
             res=r-l#abs is to get poistive output
             minheight=min(h[l],h[r])
@@ -21,9 +26,7 @@ class Solution:
                 r-=1
             else:
                 l+=1
-                r-=1
-            
-            
+                r-=1   
         return maxwater
     
         
