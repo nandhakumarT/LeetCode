76914609631390c5fc32f3a1a96985a8d901b667
Nandhakumar T <80043272+nandhakumarT@users.noreply.github.com>
2022-08-12 10:42:50 +0530

    Time: 48 ms (48.97%), Space: 13.8 MB (94.89%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.py
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.py
@@ -1,6 +1,18 @@
 class Solution:
     def checkString(self, s: str) -> bool:
         
+        i=0
+        while s[i]=='a':
+            i+=1
+            if i==len(s):
+                break
+        while i<len(s):
+            if s[i]=='a':
+                return False
+            i+=1
+        return True
+       
+        """
         l,r=0,len(s)-1
         if s==" ":
             return False
@@ -15,5 +27,5 @@ class Solution:
                     l+=1
                 else:
                     return False
-        return True
-        
+         return True
+         """
