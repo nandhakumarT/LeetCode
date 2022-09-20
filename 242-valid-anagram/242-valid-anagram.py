540c5e08be9eeb6835ad2e88f4c5673a07c12666
Nandhakumar T <80043272+nandhakumarT@users.noreply.github.com>
2022-09-20 10:41:09 +0530

    Time: 147 ms (6.18%), Space: 14.4 MB (96.94%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -10,7 +10,20 @@ class Solution:
         """
         #successfilly worked
 
+        """
         s=sorted(s)
         t=sorted(t)
         return s==t  
-    
+        """
+        if len(s)!=len(t):
+            return False
+        count_s,count_t={},{}
+        
+        for i in range(len(s)):
+            count_s[s[i]]=count_s.get(s[i],0)+1
+            count_t[t[i]]=count_t.get(t[i],0)+1
+        for c in count_s:
+            if count_s[c]!=count_t.get(c,0):
+                return False
+        
+        return True
