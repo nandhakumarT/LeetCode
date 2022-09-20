533324e43061fc2f8c8a5690d0099f7bc090fba6
Nandhakumar T <80043272+nandhakumarT@users.noreply.github.com>
2022-09-20 15:22:07 +0530

    Time: 220 ms (24.25%), Space: 18 MB (53.98%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -6,11 +6,16 @@ class Solution:
         if len(strs)==1:
             return [strs]
         res=defaultdict(list)
-             
+        for word in strs:
+            key=''.join(sorted(word))
+            res[key]+=[word]
+        return res.values()
+        """   
+        res=defaultdict(list)
         for s in strs:
             count=[0]*26
             for c in s:
                 count[ord(c) - ord("a")]+=1
             res[tuple(count)].append(s)
         return res.values()
-                
+        """        
