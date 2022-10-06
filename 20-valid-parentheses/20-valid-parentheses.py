f2fa90dabd984f7cfec3a9859fbc4b0d25a74d2e
Nandhakumar T <80043272+nandhakumarT@users.noreply.github.com>
2022-10-06 12:21:27 +0530

    Time: 62 ms (28.07%), Space: 13.9 MB (72.31%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,20 +1,38 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         
+        
+        stack=[]
+        closetoopen={ "}":"{" , "]":"[", ")":"("}
+
+        for i in s:
+            if i in closetoopen:
+                if stack and stack[-1] == closetoopen[i]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(i)
+        return True if not stack else False
+        
+        """
+        #edge case
         if len(s)%2!=0:
             return False
         
         hash={ "(": ")","[": "]","{": "}"}
         res=[]
+        #if add s values in hash
         for i in range(len(s)):
             if s[i] in hash:
                 res.append(s[i])
             
-        
+        #if (,{,[ if this value are not present in the start of the sting
+        #then pop the value and add to last_item
             else:
                 if len(res)>0:
                     last_item=res.pop()
-                    
+        #if hash of last_item has not present in s then return false            
                     if s[i] != hash[last_item]:
                         return False
                     
@@ -22,5 +40,6 @@ class Solution:
                     return False
            
         return len(res)==0
+        """
             
        
